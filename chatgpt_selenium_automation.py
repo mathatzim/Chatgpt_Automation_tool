4d7c9811ff47a90bfcf5ad8c0782f7f8cc8d7240
Matthaios Chatzimichail <89903517+mathatzim@users.noreply.github.com>
2024-04-07 17:53:47 +0300

    Create chatgpt_selenium_automation.py

---
+++ chatgpt_selenium_automation.py
@@ -0,0 +1,12 @@
+import os
+import socket
+import threading
+import time
+
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+
+
+class ChatGPTAutomation:
