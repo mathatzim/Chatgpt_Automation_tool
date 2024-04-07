9a9bba6e067ea3ac0646343dfe3c82ce4accadd7
Matthaios Chatzimichail <89903517+mathatzim@users.noreply.github.com>
2024-04-07 17:58:00 +0300

    Define the __init__ function

--- chatgpt_selenium_automation.py
+++ chatgpt_selenium_automation.py
@@ -10,3 +10,22 @@ from selenium.webdriver.common.keys import Keys
 
 
 class ChatGPTAutomation:
+  def __init__(self, chrome_path, chrome_driver_path):
+        """
+        This constructor automates the following steps:
+        1. Open a Chrome browser with remote debugging enabled at a specified URL.
+        2. Prompt the user to complete the log-in/registration/human verification, if required.
+        3. Connect a Selenium WebDriver to the browser instance after human verification is completed.
+
+        :param chrome_path: file path to chrome.exe (ex. C:\\Users\\User\\...\\chromedriver.exe)
+        :param chrome_driver_path: file path to chrome.exe (ex. C:\\Users\\User\\...\\chromedriver.exe)
+        """
+
+        self.chrome_path = chrome_path
+        self.chrome_driver_path = chrome_driver_path
+
+        url = r"https://chat.openai.com"
+        free_port = self.find_available_port()
+        self.launch_chrome_with_remote_debugging(free_port, url)
+        self.wait_for_human_verification()
+        self.driver = self.setup_webdriver(free_port)
